c4ff2c2db31ead4cd28a14f6c8075cbb5789475f
Karyne Furlan <141533267+Kaaryne@users.noreply.github.com>
2025-04-10 12:26:49 +0000

    oi

--- web/super_loja.php
+++ web/super_loja.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 <body class="container mt-5 w-75 m-auto">
@@ -21,10 +22,10 @@
             ?>
             <div class="col-4 mb-5">
                 <div class="card" style="width: 18rem;">
-                <img src="<?=$imagens[$i]?>" class="card-img-top" alt="...">
+                <img src="img/<?=$imagens[$i]?>" class="card-img-top tam-img" alt="...">
                     <div class="card-body">
                         <h5 class="card-title"><?=$produtos[$i]?></h5>
-                        <a href="#" class="btn btn-primary">R$<?=number_format($valores[$i],decimals: 2, decimal_separator:',')?></a>
+                        <a href="https://www.amazon.com.br/s?k=<?=$produtos[$i]?>" class="btn btn-primary">R$<?=number_format($valores[$i],decimals: 2, decimal_separator:',')?></a>
                     </div>
                 </div>
             </div>
