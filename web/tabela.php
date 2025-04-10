66d3e3ccbf0fd2f0c1216e938705e5497314dd1a
Karyne Furlan <141533267+Kaaryne@users.noreply.github.com>
2025-04-10 12:54:21 +0000

    sa

--- web/tabela.php
+++ web/tabela.php
@@ -7,7 +7,41 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Document</title>
 </head>
-<body>
-    
+<body class="container mt-5 w-75 m-auto">
+    <?php
+        $ingredientes = ["carne", "sal", "carvão", "pão"];
+        $quantidades = [20, 1, 5, 100];
+        $valor = [25, 4, 6, 1.5];
+    ?>    
+<table class="table">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Ingrediente</th>
+                <th>Quantidade</th>
+                <th>Valor</th>
+                <th>Total</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                for ($i=0; $i < count($valor) ; $i++) { 
+            ?>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            <?php
+                }
+            ?>
+            <tr>
+
+            </tr>
+        </tbody>
+        <tr></tr>
+    </table>
 </body>
 </html>
