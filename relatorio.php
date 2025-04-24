d5c0d3d3f5eb41f2325386d181b9572a381d86a5
Karyne Furlan <141533267+Kaaryne@users.noreply.github.com>
2025-04-24 11:41:55 +0000

    ds

---
+++ relatorio.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body class ="container mt-5 w-75 m-auto">
+    <h3 class="text-center">Lista de Tarefas</h3>
+</body>
+</html>
