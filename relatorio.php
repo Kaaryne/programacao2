4e66e9f18bec45a36a4d6752d1129bf5ee7d9e58
Karyne Furlan <141533267+Kaaryne@users.noreply.github.com>
2025-04-28 16:38:33 +0000

    o

--- relatorio.php
+++ relatorio.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body class ="container mt-5 w-75 m-auto">
@@ -29,7 +28,7 @@
                 for ($i=0; $i < count($tarefas) ; $i++) {
             ?>
                 <div class="col-4 mb-5 px-5 ">
-                    <div class="card " style="width: 25rem; height: 15rem;">
+                    <div class="card " style="width: 25rem;">
                         <div class="card-body">
                             <h5 class="card-title"><?=$tarefas[$i]['titulo']?></h5>
                             <p class="card-text"><?=$tarefas[$i]['descricao']?></p>
